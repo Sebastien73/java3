e13fcd7cf58a38c6dee063d28ef3b728781a9768
sebastien jacquelin <sebastien.jacquelin@le-campus-numerique.fr>
2023-09-11 16:57:11 +0200

    Config javadoc

--- src/dungeon/game/Game.java
+++ src/dungeon/game/Game.java
@@ -6,6 +6,8 @@ public class Game {
 
     private Characters character;
     private boolean isWarrior;
+
+
     public void play(Characters character, boolean isWarrior) {
         System.out.println("STARTING GAME");
     }
--- src/dungeon/menus/Menu.java
+++ src/dungeon/menus/Menu.java
@@ -11,10 +11,22 @@ public class Menu {
     private final Scanner scanner = new Scanner(System.in);
     private ArrayList<Warrior> warriorsList;
     private ArrayList<Mage> wizardsList;
+
+    /**
+     * @param question string
+     * @return scanner.next()
+     * Get next result of prompt for switch options
+     */
     private String getResult(String question) {
         System.out.println(question);
         return scanner.next();
     }
+
+    /**
+     * @param question string
+     * @return scanner.next()
+     * Get int for prompt in switch menu options
+     */
     public  int getIntResult(String question) {
         try {
             return Integer.parseInt(getResult(question));
@@ -23,13 +35,26 @@ public class Menu {
         }
     }
 
+    /**
+     * @return new Warrior()
+     * Invoke a new Warrior
+     */
     public Warrior createWarrior(){
         return new Warrior();
     }
+
+    /**
+     * @return new Mage()
+     * Invoke a new Mage
+     */
     public Mage createWizard(){
         return new Mage();
     }
 
+    /**
+     * @param question string
+     * @return string result
+     */
     private  String getStringResult(String question){
         String a = getResult(question);
         try{
@@ -40,6 +65,10 @@ public class Menu {
         return getStringResult(question);
     }
 
+    /**
+     * Principal menu
+     * Create character or exit program
+     */
     public void menuPrincipal(){
         int create = getIntResult("Principal menu : \n1 for create character\n2 for left the game"); //// On déclare la variable selector1 de type INT avec comme valeur l'attente d'un entier pour la variable clavier
         switch (create) { // On initialise un nouveau switch
@@ -52,6 +81,11 @@ public class Menu {
         }
     }
 
+    /**
+     * Menu of creation of character
+     * Back to previously menu
+     * Exit program
+     */
     public void createCharacter() {
         int create = getIntResult("Select type character : \n1 for Warrior\n2 for Wizard\n3 for back previously menu\n4 for left the game"); //// On déclare la variable selector1 de type INT avec comme valeur l'attente d'un entier pour la variable clavier
         switch (create) { // On initialise un nouveau switch
@@ -68,18 +102,34 @@ public class Menu {
         }
     }
 
+    /**
+     * Select warrior type
+     * Call infosOfCharacter
+     */
     private void warriorSelect() {
         System.out.println("Warrior selected");
         Warrior warrior = createWarrior();
         infosOfCharacter(warrior,true);
     }
 
+    /**
+     * Select mage type
+     * Call infosOfCharacter
+     */
     private void wizardSelect() {
         System.out.println("Wizard selected");
         Mage mage = createWizard();
         infosOfCharacter(mage,false);
     }
 
+    /**
+     *
+     * @param character Characters
+     * @param isWarrior boolean
+     * Change infos of the character previously created or play game directly
+     * Back to the principal menu
+     * Exit program
+     */
     private void infosOfCharacter(Characters character, boolean isWarrior) {
         int setInfo = getIntResult("Indicate if you want to custom your " + (isWarrior ? "warrior" : "wizard") + " \n1 for Yes\n2 for No\n3 for back previously menu\n4 for left the game");
         switch (setInfo) {
@@ -96,10 +146,23 @@ public class Menu {
         }
     }
 
+    /**
+     *
+     * @param character Characters
+     * @param isWarrior boolean
+     * Call setParameters method
+     */
     private void setInfosOfCharacter(Characters character, boolean isWarrior) {
         setParameters(character,isWarrior);
     }
 
+    /**
+     *
+     * @param character Characters
+     * @param isWarrior boolean
+     * Call different method to set attributes of character
+     * Last method called : readyToPlay
+     */
     private void setParameters(Characters character, boolean isWarrior) {
         setNameCharacter(character);
         setPointOfLifeCharacter(character, isWarrior);
@@ -107,6 +170,11 @@ public class Menu {
         readyToPlay(character,isWarrior);
     }
 
+    /**
+     *
+     * @param character Character
+     * Set a name for current character
+     */
     private void setNameCharacter(Characters character) {
         String nameCharacter = getStringResult("Enter name for your character");
 
@@ -117,6 +185,11 @@ public class Menu {
         }
     }
 
+    /**
+     * @param character Characters
+     * @param isWarrior boolean
+     * Set point of life for current character
+     */
     private void setPointOfLifeCharacter(Characters character, boolean isWarrior) {
         int pointOfLife = getIntResult("Enter number for points of life " + (isWarrior ? "(between 5 and 10)" : "(between 3 and 6)"));
         if (isWarrior) {
@@ -132,6 +205,11 @@ public class Menu {
         character.setPointLife(pointOfLife);
     }
 
+    /**
+     * @param character Characters
+     * @param isWarrior boolean
+     * Set points of strength for current character
+     */
     private void setPointOfAttackCharacter(Characters character, boolean isWarrior){
         int pointAtk = getIntResult("Enter number for attack's points" + ((isWarrior) ? "(between 5 and 10)" : "(between 8 and 15)"));
         if (isWarrior) {
@@ -147,6 +225,12 @@ public class Menu {
         character.setPointAttack(pointAtk);
     }
 
+    /**
+     * @param character Characters
+     * @param isWarrior boolean
+     * Invoke a new object Game
+     * Call play method with the current character and boolean
+     */
     private void readyToPlay(Characters character, boolean isWarrior) {
         Game game = new Game();
         game.play(character,isWarrior);
